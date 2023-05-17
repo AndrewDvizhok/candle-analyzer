c1c3f603df2164c3c57319feac78391f4f3c5d83
AndrewDvizhok <AndrewDvizhok>
2023-05-17 18:50:59 +0000

    Initial commit

---
+++ src/test/groovy/io/andrewdvizhok/CandleAnalyzerTest.groovy
@@ -0,0 +1,19 @@
+package io.andrewdvizhok
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class CandleAnalyzerTest {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Test
+    void testItWorks() {
+        assert application.running == true
+    }
+
+}
